ab3cd5ec4782a047d8019df4e774127ceed85ca2
Ryan <wilkinsrs@outlook.com>
2016-05-03 01:52:27 +0000

    add lots of pretty colors

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,18 +2,21 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get :home
+    get :home #the symbol here maps directly to the routes.rb file, where we defined "home" etc.
     assert_response :success
+    assert_select "title", "beermac | home"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "beermac | about"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "beermac | contact"
   end
 
 end
